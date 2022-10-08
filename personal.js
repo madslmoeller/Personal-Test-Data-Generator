c91992e1644afdcba10a6572f3ce9fb2e6c9e2e3
Mads <madsmoeller1995@hotmail.com>
2022-10-08 14:05:02 +0200

    setup

---
+++ personal.js
@@ -0,0 +1,28 @@
+class Personal {
+  constructor(cpr, fullName, gender, dateOfBirth, address, phoneNumber) {
+    this.cpr = cpr;
+    this.fullName = fullName;
+    this.gender = gender;
+    this.dateOfBirth = dateOfBirth;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+  }
+}
+
+function cpr() {}
+
+function fullnameAndGender() {}
+
+function fullnameGenderAndBirth() {}
+
+function cprFullnameAndGender() {}
+
+function cprFullnameGenderAndBirth() {}
+
+function address() {}
+
+function phoneNumber() {}
+
+function allInformation() {}
+
+function informationInBulk() {}

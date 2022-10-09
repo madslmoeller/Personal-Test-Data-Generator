4e2cb20ad04a1400683ec21806feb354267336be
KennetTech <64315533+KennetTech@users.noreply.github.com>
2022-10-09 21:50:30 +0200

    function fullnameAndGender nearly contructed

--- personal.js
+++ personal.js
@@ -1,3 +1,4 @@
+
 class Personal {
   constructor(cpr, fullName, gender, dateOfBirth, address, phoneNumber) {
     this.cpr = cpr;
@@ -9,11 +10,44 @@ class Personal {
   }
 }
 
+const person = new Personal();
+
 function cpr() {}
 
-function fullnameAndGender() {}
 
-function fullnameGenderAndBirth() {}
+
+function fullnameAndGender() {
+  //reading json file to varible --- Think we need a method for this. it is repeated more then once.
+  const fs = require('fs')
+  let json = fs.readFileSync("./personname.json")
+  let jsonify = JSON.parse(json)
+
+  //Need random number method here - to pase in to the next section
+
+  //Taking the randomNumber variable from method and getting first name, last name and gender
+  // Gender is set to object, where first name and last name are added together to make fullname in object
+  const firstName = jsonify.persons[randomNumber].name;
+  const lastName = jsonify.persons[randomNumber].surname;
+  person.gender = jsonify.persons[randomNumber].gender;
+  person.fullName = firstName + " " + lastName;
+  
+  //writing result to console 
+  console.log(person.fullName + " " + person.gender);
+
+  // returning the result as a string
+  return firstName + " " + lastName + " " + person.gender;
+
+}
+
+function fullnameGenderAndBirth() {
+    
+    //calling fullnameAndGender() for fullname and gender, then adding cpr method, where only the first 6 charaters af taken.
+    const fullnameGenderBirth = fullnameAndGender() + " " + cpr().slice(0, 6);
+
+    console.log(fullnameGenderBirth)
+
+    return fullnameGenderAndBirth;
+}
 
 function cprFullnameAndGender() {}
 
